274b663adaff94bda15c521dcc6ce1e870eb62ef
yangbnag <yanbam@naver.com>
2022-05-19 18:18:19 +0900

    22.05.19

--- 2.js_study/1.core-js/8.함수/3.js
+++ 2.js_study/1.core-js/8.함수/3.js
@@ -1,25 +1,12 @@
 
-var solutionNum = '';
 
-function calculator(x) {
-    for (let i = 1; i <= x; i++) {
-        // console.log(5);
-
-        if (x % i === 0) {
-            solutionNum += `${i} \n`;
-        }
-    }
+function add() {
+    console.log('함수 실행!!');
+    return 1+2;
 }
+console.log('==============');
+var result = add()
+console.log('================');
+console.log(result);
 
-console.log(calculator(7));
 
-
-/*
-let calculator1 = 5%1;
-let calculator2 = 5%2;
-let calculator3 = 5%3;
-let calculator4 = 5%4;
-let calculator5 = 5%5;
-
-console.log(calculator1,calculator2,calculator3,calculator4,calculator5);
-*/
